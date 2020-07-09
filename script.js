de80b5819796480e7187148987a03ee38e917764
shawon39 <shshawon39@gmail.com>
2020-07-09 22:55:10 +0600

    Added multiple reducers

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const redux = require("redux");
 const createStore = redux.createStore;
+const combineReducers = redux.combineReducers
 
 const BUY_CAKE = "BUY_CKE";
+const BUY_ICE_CREAM = "BUY_ICE_CREAM";
 
 function buyCake() {
 	return {
@@ -10,8 +12,19 @@ function buyCake() {
 	};
 }
 
+function buyIceCream() {
+	return {
+		type: BUY_ICE_CREAM,
+		info: "Second redux action",
+	};
+}
+
 const initialState = {
 	numOfCakes: 10,
+};
+
+const initialState2 = {
+	numOfIceCream: 20,
 };
 
 const reducer = (state = initialState, action) => {
@@ -28,7 +41,26 @@ const reducer = (state = initialState, action) => {
 	}
 };
 
-const store = createStore(reducer);
+const reducer2 = (state = initialState2, action) => {
+	switch (action.type) {
+		case BUY_ICE_CREAM:
+			return {
+				...state,
+				numOfIceCream: state.numOfIceCream - 1,
+			};
+
+		default: {
+			return state;
+		}
+	}
+};
+
+const rootReducers = combineReducers({
+	cake : reducer,
+	iceCream : reducer2
+})
+
+const store = createStore(rootReducers);
 console.log("Initial state: ", store.getState());
 const unsubscribe = store.subscribe(() =>
 	console.log("Updated store", store.getState())
@@ -36,4 +68,5 @@ const unsubscribe = store.subscribe(() =>
 store.dispatch(buyCake());
 store.dispatch(buyCake());
 store.dispatch(buyCake());
+store.dispatch(buyIceCream());
 unsubscribe();
